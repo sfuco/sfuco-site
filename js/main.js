862d6a8053446efda61df4ca25855ceb7c12a390
Reinier E <r.evangel@live.com>
2018-03-19 01:52:29 -0400

    add mobile nav functions

--- js/main.js
+++ js/main.js
@@ -7,6 +7,32 @@
 
 // Function Definitions
 ///////////////////////////////////////
+// Returns true if the mobile nav window
+// is down
+function navIsDown() {
+  return ($('footer .nav-container').hasClass('.down'));
+};
+
+// Loads the cooresponding nav based on which footer
+// button was pressed
+function loadNav(id) {
+  if (!navIsDown()) {
+    $('footer .nav-container').addClass('down');
+  }
+
+  $('footer .nav-container nav').removeClass('active');
+
+  if (id == 'tab1') {
+    $('footer #pages').addClass('active');
+
+  } else if (id == 'tab2') {
+    $('footer #media').addClass('active');
+
+  } else if (id == 'tab3') {
+    $('footer #social').addClass('active');
+  }
+
+}
 
 // Events
 ////////////////////////////////////////
@@ -15,6 +41,10 @@ $(document).ready(function() {
   // Play loading screen animation when page loads
   $(window).load(function() {
     $('.loading-screen').fadeOut('slow');
+  });
+
+  $('footer input').on('click', function() {
+    loadNav(this.id);
   });
 
 });
